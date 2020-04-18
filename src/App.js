e6a6f4c96ecb872d74d03380a4e45bbfc58e9f77
Alex <alexsmith11nd@gmail.com>
2020-04-18 23:13:51 +0100

    testing photo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import elliot from './elliot.jpg';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <img src={elliot} className='App-logo' alt='elliot' />
         <p>I win, Elliot eat my ass</p>
         {/* <a
           className='App-link'
