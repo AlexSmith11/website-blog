7efb16143b09be60f227b1d37c1c155982681c30
Alex <alexsmith11nd@gmail.com>
2020-04-18 21:16:28 +0100

    Added prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <div className='App'>
+      <header className='App-header'>
         <p>Matty sucks, worst player</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {/* <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
