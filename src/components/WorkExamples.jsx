d54edefedfd2d445c8819de8832863e70b83ea58
Alex <alexsmith11nd@gmail.com>
2021-06-03 19:01:27 +0100

    applied lightbox to project page

--- src/components/WorkExamples.jsx
+++ src/components/WorkExamples.jsx
@@ -1,28 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
 import '../styles/photos.css';
 import '../styles/photo-grid.css';
+import LightboxHandler from '../components/lightbox';
 
 const WorkExamples = () => {
+  const [check, setCheck] = useState(false);
+  const [imgUrl, changeUrl] = useState([]);
+
+  const handleImageClick = url => {
+    checker();
+    changeUrl(url);
+  };
+
+  const checker = () => {
+    // need to check if another image is already open first (?)
+    setCheck(prevCheck => !prevCheck);
+  };
+
   return (
-    <div className='basic-grid'>
-      <div
-        className='card'
-        style={{
-          backgroundImage: "url('https://i.imgur.com/qeHAKGy.png')"
-        }}>
-        <a></a>
+    <div>
+      <div className='lightboxThing'>
+        {check && <LightboxHandler onClick={() => checker} imgUrl={imgUrl} />}
       </div>
-      <div
-        className='card'
-        style={{
-          backgroundImage: "url('https://i.imgur.com/6rtHWlz.png')"
-        }}></div>
-      <div
-        className='card'
-        style={{
-          backgroundImage: "url('https://i.imgur.com/q9eXph0.png')"
-        }}></div>
+
+      <div className='basic-grid'>
+        <div
+          onClick={() => handleImageClick('https://i.imgur.com/qeHAKGy.png')}
+          className='card'
+          style={{
+            backgroundImage: "url('https://i.imgur.com/qeHAKGy.png')"
+          }}>
+          <a></a>
+        </div>
+        <div
+          onClick={() => handleImageClick('https://i.imgur.com/6rtHWlz.png')}
+          className='card'
+          style={{
+            backgroundImage: "url('https://i.imgur.com/6rtHWlz.png')"
+          }}></div>
+        <div
+          onClick={() => handleImageClick('https://i.imgur.com/q9eXph0.png')}
+          className='card'
+          style={{
+            backgroundImage: "url('https://i.imgur.com/q9eXph0.png')"
+          }}></div>
+      </div>
     </div>
   );
 };
